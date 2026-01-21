16c3cd6d537cbdd7791e3076d54c45e930b665b2
Hosein <hosein.hasani@sharif.edu>
2026-01-21 22:47:01 +0330

    Big update

--- beam search codes/beam_search_nltk_bayesian.py
+++ beam search codes/beam_search_nltk_bayesian.py
@@ -24,6 +24,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from classifier.mlp_deployment import FPAttentionClassifier
 
+from collections import defaultdict, Counter
 
 from transformers import CLIPProcessor, CLIPModel
 from PIL import Image
@@ -36,158 +37,301 @@ import matplotlib.pyplot as plt
 from llava.constants import IMAGE_TOKEN_INDEX
 import numpy as np
 import re
+import spacy
+nlp = spacy.load("en_core_web_sm", disable=["ner", "parser"])
+
 
 import re
 from typing import List
 
-def find_llava_indices(gen_text: str, chair_tokens: List[List[str]], subwords) -> List[List[int]]:
 
-    results = []
-
-    # --- Build offsets by walking the text sequentially ---
-    offsets = []
-    pointer = 0
-    text_lower = gen_text.lower()
-    for sw in subwords:
-        sw_clean = sw.strip()
-        if not sw_clean:
-            offsets.append((pointer, pointer))
-            continue
-
-        # Find sw starting from current pointer
-        idx = text_lower.find(sw_clean.lower(), pointer)
-        if idx == -1:
-            start, end = pointer, pointer + len(sw_clean)
+def charpos_to_lemma(char_pos, word_spans):
+    for word, lemma, start, end in word_spans:
+        if char_pos == 0:
+            return lemma
         else:
-            start, end = idx, idx + len(sw_clean)
-        offsets.append((start, end))
-        pointer = end
+            if start-1 == char_pos:
+                return lemma
+    return None
 
-    # --- Collect matches for each word ---
-    matches_by_word = {}
-    for pair in chair_tokens:
-        target = pair[0]
-        key = target.lower()
-        if key not in matches_by_word:
-            pattern = rf"\b{re.escape(target)}(es|s)?\b"
-            matches_by_word[key] = [m.span() for m in re.finditer(pattern, gen_text, flags=re.IGNORECASE)]
+def sort_select(scores):
+    return np.sort(scores[0])[-200:]
 
-    consumed_idx = {k: 0 for k in matches_by_word}
+def collect_scores_by_indices(score_records, llava_indices):
+    collected = []
+    for inds in llava_indices:
+        for i in inds:
+            if i < len(score_records):
+                collected.append([i, sort_select(score_records[i].cpu().numpy())])
+            break
+    return collected
 
-    # --- Sequentially assign matches ---
-    for pair in chair_tokens:
-        target = pair[0]
-        key = target.lower()
-        spans = matches_by_word.get(key, [])
-        idx = consumed_idx[key]
-
-        if idx >= len(spans):
-            results.append([])
-            continue
-
-        start_char, end_char = spans[idx]
-
-        # Collect all token indices overlapping this match
-        indices = [
-            i for i, (tok_start, tok_end) in enumerate(offsets)
-            if tok_start < end_char and tok_end > start_char
-        ]
-
-        results.append(indices)
-        consumed_idx[key] += 1
-
-    return results
-
-
-
-def extract_top_attentions_by_steps(all_attentions, token_indices, image_token_index, image_tokens_count,
-                                    topk=5, layer_start=0, layer_end=14, aggregate=True):
-
-    results = {}
-    batch_size = all_attentions[0][0].shape[0]
-    assert batch_size == 1, "Only batch size 1 supported."
-
-    n_layers_total = len(all_attentions[0])
-    layer_end = min(layer_end, n_layers_total - 1)
-    
-    token_indices = [inds[0] for inds in token_indices if len(inds) > 0]
-
-    for idx in token_indices:
-
-        # (num_layers, B, H, S, S)
-        try:
-            attn_matrix = all_attentions[idx][layer_start:layer_end + 1]
-        except:
-            print("Error",idx)
-            
-        target_attn_vec = [attn_matrix[i][0].cpu().numpy() for i in range(len(attn_matrix)) ]  
-        target_attn_vec = np.array(target_attn_vec).squeeze()
-
-        image_attn = target_attn_vec[..., image_token_index:image_token_index + image_tokens_count]
-
-        topk_indices = np.argsort(image_attn, axis=-1)[..., -topk:][..., ::-1]
-
-        topk_values = np.take_along_axis(image_attn, topk_indices, axis=-1)
-
-        results[idx] = topk_values
-
-    return results
-
-
+def topk_batch(attn, k):
+    # attn: (H, N)
+    k = min(k, attn.shape[-1])
+    vals, idx = torch.topk(attn, k=k, dim=-1)
+    return vals, idx
 
 
 def sentence_evaluator(
     candidate_attentions,
+    candidate_logits,
     classifier,
     token_offset,
     tokenizer,
     output_ids=None,
-    image_token_index=35,
-    image_tokens_count=576,
+    image_start_idx=35,
+    image_length=576,
     topk=20,
     layer_start=0,
     layer_end=32,
     use_nltk=False,  
 ):
-    res_len = len(candidate_attentions)
-    if use_nltk:
-        generated_text = tokenizer.batch_decode(
-            output_ids[:, -len(candidate_attentions):], skip_special_tokens=True
-        )[0]
-        subwords = [tokenizer.decode([tid]) for tid in output_ids[0][-(res_len+10):]]
-        subwords = subwords[-res_len:]
+    
+    samples_4_classifier = []
+    
+    num_layers, num_heads, TOPK = 32, 32, 20
+    
+    sentence_len = len(candidate_attentions)
+    
+    scores = candidate_logits
+    # tp_scores = collect_scores_by_indices(scores, tp_llava_indices)
+    
+    token_len = output_ids.shape[1]
+    output_attentions = candidate_attentions
 
-        words = nltk.word_tokenize(generated_text.lower())
-        tagged_sent = nltk.pos_tag(words)
-        nouns = [[word,word] for word, tag in tagged_sent if "NN" in tag]
+    # Decode generated text
+    # gen_text = tokenizer.batch_decode(
+    #     output_ids[:, -sentence_len:], skip_special_tokens=True
+    # )[0]
 
-        noun_indices = find_llava_indices(generated_text, nouns, subwords)
-        all_indices = noun_indices
+    current_ids = output_ids[:, -(sentence_len+token_offset):]
+    gen_text = tokenizer.batch_decode(
+        current_ids, skip_special_tokens=True
+    )[0]
+    
+    subwords = [tokenizer.decode([tid]) for tid in current_ids[0]]
 
-    else:
-        all_indices = [[i] for i in range(res_len)]
+    words = nltk.word_tokenize(gen_text.lower())
+    tagged_sent = nltk.pos_tag(words)
+    nouns = [[word,word] for word, tag in tagged_sent if "NN" in tag]
 
-    all_attn = extract_top_attentions_by_steps(
-        candidate_attentions,
-        all_indices,
-        image_token_index,
-        image_tokens_count,
-        topk=topk,
-        layer_start=layer_start,
-        layer_end=layer_end,
+        
+
+    def word_to_first_token_id(word):
+        ids = tokenizer(
+            word,
+            add_special_tokens=False
+        )["input_ids"]
+        return ids[0] if len(ids) > 0 else None
+
+
+    token_ids_set = set()
+    for w in words:
+        tid = word_to_first_token_id(w)
+        if tid is not None:
+            token_ids_set.add(tid)
+
+
+    doc = nlp(gen_text)
+
+
+    lemmas = []
+    word_spans = []  # (lemma, start_char, end_char)
+
+    for token in doc:
+        if token.is_space or token.is_punct:
+            continue
+        lemmas.append(token.lemma_.lower())
+        word_spans.append((token.text, token.lemma_.lower(), token.idx, token.idx + len(token.text)))
+
+    lemma_total_counter = Counter(lemmas)
+    
+    lemma_occurrence_counter = defaultdict(int)
+    lemma_to_indices = defaultdict(list)
+
+
+
+    selected_noun_indices = []
+    
+
+    # --- tokenizer offsets for generated text ---
+
+    fast_tokenizer = AutoTokenizer.from_pretrained(
+            tokenizer.name_or_path,
+            use_fast=True
+        )
+    
+    enc = fast_tokenizer(
+        gen_text,
+        return_offsets_mapping=True,
+        add_special_tokens=False
     )
-    n_fp = [0]
-    if len(all_attn)>0:
-        shifted_attn = {k + token_offset: v for k, v in all_attn.items()}
-        preds = classifier.predict(shifted_attn)
+
+    token_ids = enc["input_ids"]
+    offsets = enc["offset_mapping"]  # list of (start_char, end_char) per subtoken
+
+    # --- FIRST PASS: collect lemma → token indices ---
+    for idx, (start_char, end_char) in enumerate(offsets):
+        if start_char == end_char:
+            continue
+
+        lemma = charpos_to_lemma(start_char, word_spans)
+        if lemma is None:
+            continue
+
+        lemma_to_indices[lemma].append(idx)
 
 
-        for k, p in preds.items():
-            if 3 <= k <= 160:
-                if p > 0.5:
-                    n_fp.extend([1])
+    def first_subtoken_map(words, tokenizer):
+        """
+        Returns a dict mapping each word to a set of plausible first subtoken IDs,
+        including common inflectional variants (plural, -ed, -ing).
+        """
+        mapping = {}
+        for word in words:
+            token_ids_set = set()
+            # include original word and common variants
+            variants = [word, word + 's', word + 'es', word + 'ed', word + 'ing']
+            for var in variants:
+                ids = tokenizer(var, add_special_tokens=False)["input_ids"]
+                if len(ids) > 0:
+                    token_ids_set.add(ids[0])
+            mapping[word] = token_ids_set
+        return mapping
+    
 
-    return -np.sum(n_fp)
+    gen_text_first_subtoken_map = first_subtoken_map(words, tokenizer)
+
+
+    gen_text_lemmas = set()
+    for w in words:
+        doc_w = nlp(w)
+        for t in doc_w:
+            if not t.is_space and not t.is_punct:
+                gen_text_lemmas.add(t.lemma_.lower())
+
+
+    selected_noun_positions = set()
+
+    seen_lemmas_per_word = defaultdict(bool)
+    skip_next = set()  # indices to skip
+
+    for idx, (token_id, (start_char, end_char)) in enumerate(zip(token_ids, offsets)):
+            if tokenizer.decode([token_id]) == "cars":
+                print("hi")
+            if start_char == end_char:
+                continue  # whitespace or special
+
+            lemma = charpos_to_lemma(start_char, word_spans)
+            if lemma is None:
+                continue
+
+            # Skip subtokens that are in skip_next
+            if idx in skip_next:
+                continue
+
+            # Only increment occurrence on the first subtoken of the word
+            if not seen_lemmas_per_word.get(start_char, False):
+                lemma_occurrence_counter[lemma] += 1
+                seen_lemmas_per_word[start_char] = True
+
+
+            occurrence_num = lemma_occurrence_counter[lemma]
+            total_reps = lemma_total_counter[lemma]
+
+                # check hallucinated words
+            for word, first_id in gen_text_first_subtoken_map.items():
+                word_lemma = nlp(word)[0].lemma_.lower()  # first lemma of word
+                if lemma == word_lemma and token_id in first_id:
+                    selected_noun_positions.add(idx)
+
+
+            if idx in selected_noun_positions:
+                token_type = "noun"
+                selected_noun_indices.append([idx])
+                word_ids = tokenizer(lemma, add_special_tokens=False)["input_ids"]
+                skip_next.update(idx + i for i in range(1, len(word_ids)+1))
+
+
+
+            topk_image_indices = np.zeros((num_layers, num_heads, TOPK), dtype=np.int64)
+            topk_image_values  = np.zeros((num_layers, num_heads, TOPK), dtype=np.float32)
+
+            topk_text_indices  = np.zeros((num_layers, num_heads, TOPK), dtype=np.int64)
+            topk_text_values   = np.zeros((num_layers, num_heads, TOPK), dtype=np.float32)
+
+            image_sum = np.zeros((num_layers, num_heads), dtype=np.float32)
+            text_sum  = np.zeros((num_layers, num_heads), dtype=np.float32)
+
+            topk_image_indices_next = np.zeros((num_layers, num_heads, TOPK), dtype=np.int64)
+            topk_image_values_next  = np.zeros((num_layers, num_heads, TOPK), dtype=np.float32)
+
+            topk_text_indices_next  = np.zeros((num_layers, num_heads, TOPK), dtype=np.int64)
+            topk_text_values_next   = np.zeros((num_layers, num_heads, TOPK), dtype=np.float32)
+
+            for layer_idx in range(num_layers):
+                target_attn_vec = output_attentions[idx][layer_idx][...,-1, :]  #latest query           # (H, Q, K)
+
+
+                img_attn = target_attn_vec[..., image_start_idx:image_start_idx + image_length][0]
+
+                text_attn_before_img = target_attn_vec[...,:image_start_idx]
+                text_attn_after_img = target_attn_vec[...,image_start_idx + image_length:]
+                txt_attn =torch.cat([text_attn_before_img, text_attn_after_img], dim=-1)[0]                 # (H, T)
+
+                # sums
+                image_sum[layer_idx] = img_attn.sum(dim=-1).cpu().numpy()
+                text_sum[layer_idx]  = txt_attn.sum(dim=-1).cpu().numpy()
+
+                # top-k
+                img_vals, img_idx = topk_batch(img_attn, TOPK)
+                txt_vals, txt_idx = topk_batch(txt_attn, TOPK)
+                #txt index inja nesbi nis 
+                topk_image_indices[layer_idx] = img_idx.cpu().numpy()
+                topk_image_values[layer_idx]  = img_vals.cpu().numpy()
+
+                topk_text_indices[layer_idx]  = txt_idx.cpu().numpy()
+                topk_text_values[layer_idx]   = txt_vals.cpu().numpy()
+
+                # ---------- NEXT TOKEN ----------
+                if idx+1 < len(output_attentions):
+                    target_attn_vec_nxt = output_attentions[idx+1][layer_idx][...,-1, :]  #latest query           # (H, Q, K)
+
+                    img_attn_nxt = target_attn_vec_nxt[..., image_start_idx:image_start_idx + image_length][0]
+
+                    text_attn_before_img_nxt = target_attn_vec_nxt[...,:image_start_idx]
+                    text_attn_after_img_nxt = target_attn_vec_nxt[...,image_start_idx + image_length:]
+                    txt_attn_nxt =torch.cat([text_attn_before_img_nxt, text_attn_after_img_nxt], dim=-1)[0]                 # (H, T)
+
+                    # top-k
+                    img_vals_nxt, img_idx_nxt = topk_batch(img_attn_nxt, TOPK)
+                    txt_vals_nxt, txt_idx_nxt = topk_batch(txt_attn_nxt, TOPK)
+                    #txt index inja nesbi nis 
+                    topk_image_indices_next[layer_idx] = img_idx_nxt.cpu().numpy()
+                    topk_image_values_next[layer_idx]  = img_vals_nxt.cpu().numpy()
+
+                    topk_text_indices_next[layer_idx]  = txt_idx_nxt.cpu().numpy()
+                    topk_text_values_next[layer_idx]   = txt_vals_nxt.cpu().numpy()
+
+            
+            sample_dict = {
+                "token_idx": idx,
+                "topk_attn": topk_image_values.copy(),
+                "topk_attn_next": topk_image_values_next.copy(),
+                "logits": None,
+                "total_reps": total_reps,
+                "rep_num": occurrence_num,
+                "token_id": token_id
+                }
+
+
+            samples_4_classifier.append(sample_dict)
+        
+
+    preds = classifier.predict([samples_4_classifier])
+
+    return preds
 
 
 
@@ -303,13 +447,12 @@ def sample_with_ensemble(
             streamer.put(next_tokens.cpu())
 
     while not finished:
-        candidate_outputs, candidate_attentions, candidate_scores, candidate_model_kwargs = [], [], [], []
+        candidate_outputs, candidate_attentions, candidate_logits, candidate_scores, candidate_model_kwargs = [], [], [], [], []
 
         for candidate_idx in range(ensemble_size):
             model_kwargs_tmp = _detach_and_clone_model_kwargs(model_kwargs_main, device=next(self.parameters()).device)
             input_tmp = final_input_ids.clone()
-            decoder_attn_tmp, token_count, eos_hit = [], 0, False
-            diverse_enough = True  # assume candidate is diverse
+            decoder_attn_tmp, decoder_lgt_tmp, token_count, eos_hit = [], [], 0, False
 
             while token_count < pseudo_sentence_length and not eos_hit:
                 model_inputs = self.prepare_inputs_for_generation(input_tmp, **model_kwargs_tmp)
@@ -320,6 +463,9 @@ def sample_with_ensemble(
                 next_token_scores = logits_warper(input_tmp, next_token_scores)
                 probs = F.softmax(next_token_scores, dim=-1)
                 next_tokens = torch.multinomial(probs, num_samples=1).squeeze(1)
+                
+                # decoder_lgt_tmp.append(outputs.scores)
+                decoder_lgt_tmp.append(next_token_scores)
 
                 input_tmp = torch.cat([input_tmp, next_tokens[:, None]], dim=-1)
                 decoder_attn_tmp.append(
@@ -336,6 +482,7 @@ def sample_with_ensemble(
 
             candidate_outputs.append(input_tmp)
             candidate_attentions.append(decoder_attn_tmp)
+            candidate_logits.append(decoder_lgt_tmp)
             candidate_model_kwargs.append(model_kwargs_tmp)
 
         if len(candidate_outputs) == 0:
@@ -346,7 +493,7 @@ def sample_with_ensemble(
         # ---------- Evaluate candidates ----------
         for i in range(len(candidate_outputs)):
             candidate_scores.append(
-                sentence_evaluator(candidate_attentions[i], classifier, total_generated, tokenizer, candidate_outputs[i], use_nltk=use_nltk)
+                sentence_evaluator(candidate_attentions[i], candidate_logits[i], classifier, total_generated, tokenizer, candidate_outputs[i], use_nltk=use_nltk)
             )
 
         total_generated += pseudo_sentence_length
